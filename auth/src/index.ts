a32607024e76cee01860ba9a08078e9776a19dd6
Dhans <sgdhans@gmail.com>
2022-04-12 12:51:24 +0530

    ingress configured

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,8 +1,15 @@
 import express from "express";
 import { json } from "body-parser";
+import { currentUserRouter } from "./routes/currentUser";
+import { signinRouter } from "./routes/signin";
+import { signupRouter } from "./routes/signup";
 
 const app = express();
 app.use(json());
+
+app.use(currentUserRouter);
+app.use(signinRouter);
+app.use(signupRouter);
 
 app.listen(3000, () => {
   console.log("listening auth service at 3000 ");
