79f601f17b0078b692b83316ad792d370564a9b3
Dhans <sgdhans@gmail.com>
2022-04-16 15:59:27 +0530

    async error handling
    using package for avoiding using next()

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,9 +1,11 @@
 import express from "express";
 import { json } from "body-parser";
+import "express-async-errors";
 import { currentUserRouter } from "./routes/currentUser";
 import { signinRouter } from "./routes/signin";
 import { signupRouter } from "./routes/signup";
 import { errorHandler } from "./middlewares/errorHandler";
+import { NotFoundError } from "./errors/notFoundError";
 
 const app = express();
 app.use(json());
@@ -11,8 +13,13 @@ app.use(json());
 app.use(currentUserRouter);
 app.use(signinRouter);
 app.use(signupRouter);
+
+app.all("*", () => {
+  throw new NotFoundError();
+});
+
 app.use(errorHandler);
 
 app.listen(3000, () => {
-  console.log("listening auth service at 3000 ");
+  console.log("listening auth service at 3000!!");
 });
