edc20ae4a6517fb79664e56958e6c986c6a3b37e
Dhans <sgdhans@gmail.com>
2022-04-19 13:09:37 +0530

    unauthorized errors and more middlewares

---
+++ auth/src/errors/notAuthorized.ts
@@ -0,0 +1,12 @@
+import { CustomError } from "./customError";
+
+export class NotAuthorizedError extends CustomError {
+  statusCode = 401;
+  constructor() {
+    super("not authorized");
+    Object.setPrototypeOf(this, NotAuthorizedError.prototype);
+  }
+  serializeErrors() {
+    return [{ message: "not authorized" }];
+  }
+}
