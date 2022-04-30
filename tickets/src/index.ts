b24a9af537a1843dda8678d182b5ae2be3844a8b
Dhans <sgdhans@gmail.com>
2022-04-30 16:12:58 +0530

    tickets env config

--- tickets/src/index.ts
+++ tickets/src/index.ts
@@ -1,16 +1,21 @@
 import mongoose from "mongoose";
 import { app } from "./app";
-////
+
+////env validations
+if(!process.env.JWT_KEY) throw new Error("JWT KEY must be defined")
+if(!process.env.MONGO_URI) throw new Error("MONGO URI must be defined")
+
+
 const dbConnection = async () => {
   try {
-    await mongoose.connect("mongodb://auth-mongo-srv:27017/auth");
-    console.log("connection ok ds");
+    await mongoose.connect(process.env.MONGO_URI as string);
+    console.log("connection ok dsds");
   } catch (error) {
     console.error(error);
   }
 };
 
 app.listen(3000, () => {
-  console.log("listening auth service at 30 00");
+  console.log("listening tickets service at 30 00");
 });
 dbConnection();
