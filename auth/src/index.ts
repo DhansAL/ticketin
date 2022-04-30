256b7428fbc3777b40a39a7286ca03b03cd9ec31
Dhans <sgdhans@gmail.com>
2022-04-30 16:13:45 +0530

    tickets env config

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -2,6 +2,8 @@ import mongoose from "mongoose";
 import { app } from "./app";
 ////
 const dbConnection = async () => {
+if(!process.env.JWT_KEY) throw new Error("JWT KEY must be defined")
+
   try {
     await mongoose.connect("mongodb://auth-mongo-srv:27017/auth");
     console.log("connection ok ds");
