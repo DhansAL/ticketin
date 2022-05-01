96fa18b1b6628cefc1cf01d1d9b6beb2a4792571
Dhans <sgdhans@gmail.com>
2022-05-01 15:12:21 +0530

    working instance of all tests and suitable cookie middleware version

--- tickets/src/app.ts
+++ tickets/src/app.ts
@@ -2,7 +2,7 @@ import express from "express";
 import "express-async-errors";
 import { json } from "body-parser";
 import cookieSession from "cookie-session";
-import { errorHandler, NotFoundError, currentUser } from "@cygnetops/common";
+import { errorHandler, NotFoundError, currentUser } from "@dticketin/common";
 import { createTicketRouter } from "./routes/new";
 import { showTicketRouter } from "./routes/show";
 
--- tickets/src/test/setup.ts
+++ tickets/src/test/setup.ts
@@ -1,7 +1,6 @@
 import { MongoMemoryServer } from "mongodb-memory-server";
 import mongoose from "mongoose";
-import request from "supertest";
-import { app } from "../app";
+
 import jwt from "jsonwebtoken";
 
 declare global {
