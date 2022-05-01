afc682183697c97e4957b8a6b3bb255bb6563d6f
Dhans <sgdhans@gmail.com>
2022-05-01 15:36:39 +0530

    index route

--- tickets/src/routes/__test__/show.test.ts
+++ tickets/src/routes/__test__/show.test.ts
@@ -1,20 +1,20 @@
-import request from 'supertest';
-import { app } from '../../app';
-import mongoose from 'mongoose';
+import request from "supertest";
+import { app } from "../../app";
+import mongoose from "mongoose";
 
-it('returns a 404 if the ticket is not found', async () => {
+it("returns a 404 if the ticket is not found", async () => {
   const id = new mongoose.Types.ObjectId().toHexString();
 
   await request(app).get(`/api/tickets/${id}`).send().expect(404);
 });
 
-it('returns the ticket if the ticket is found', async () => {
-  const title = 'concert';
+it("returns the ticket if the ticket is found", async () => {
+  const title = "concert";
   const price = 20;
 
   const response = await request(app)
-    .post('/api/tickets')
-    .set('Cookie', global.signin())
+    .post("/api/tickets")
+    .set("Cookie", global.signin())
     .send({
       title,
       price,
