ca6b2e62b1a590c34369282c02a6927d29dc88dc
Dhans <sgdhans@gmail.com>
2022-05-01 10:27:33 +0530

    ticket formation

--- tickets/src/models/ticket.ts
+++ tickets/src/models/ticket.ts
@@ -13,3 +13,35 @@ interface TicketDoc extends mongoose.Document {
 interface TicketModel extends mongoose.Model<TicketDoc> {
   build(creds: TicketCreds): TicketDoc;
 }
+
+const ticketSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+    },
+    userId: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    toJSON: {
+      transform(doc, ret) {
+        ret.id = ret._id;
+        delete ret._id;
+      },
+    },
+  }
+);
+
+ticketSchema.statics.build = (creds: TicketCreds) => {
+  return new Ticket(creds);
+};
+const Ticket = mongoose.model<TicketDoc, TicketModel>("Ticket", ticketSchema);
+
+export { Ticket };
