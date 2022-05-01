4b04a8c57c8f5e41d2f3909b2e54ba242f0d011b
Dhans <sgdhans@gmail.com>
2022-05-01 16:44:59 +0530

    update ticket test all clear

--- tickets/src/routes/__test__/update.test.ts
+++ tickets/src/routes/__test__/update.test.ts
@@ -68,4 +68,26 @@ it("returns a 400 if the user does not provide correct title or price while upda
     .expect(400);
 });
 
-it("returns a 200 if update is succesfull with good creds", async () => {});
+it("returns a 200 if update is succesfull with good creds", async () => {
+  const cookie = global.signin();
+  const response = await request(app)
+    .post(`/api/tickets`)
+    .set("Cookie", cookie)
+    .send({
+      title: "testtest",
+      price: 29,
+    });
+
+  await request(app)
+    .put(`/api/tickets/${response.body.id}`)
+    .set("Cookie", cookie)
+    .send({ title: "finnaly a good updated ticket", price: 11 })
+    .expect(200);
+
+  const ticketResponse = await request(app)
+    .get(`/api/tickets/${response.body.id}`)
+    .send();
+
+  expect(ticketResponse.body.title).toEqual("finnaly a good updated ticket");
+  expect(ticketResponse.body.price).toEqual(11);
+});
--- tickets/src/routes/update.ts
+++ tickets/src/routes/update.ts
@@ -29,6 +29,12 @@ router.put(
       throw new NotAuthorizedError();
     }
 
+    ticket.set({
+      title: req.body.title,
+      price: req.body.price,
+    });
+    await ticket.save();
+
     res.send(ticket);
   }
 );
