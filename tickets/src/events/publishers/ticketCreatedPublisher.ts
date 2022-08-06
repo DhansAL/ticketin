dfa5ba1b4b1546155e52604bce294733f527d08a
Dhans <sgdhans@gmail.com>
2022-08-06 16:35:55 +0530

    tickets - publisher created

--- tickets/src/events/publishers/ticketCreatedPublisher.ts
+++ tickets/src/events/publishers/ticketCreatedPublisher.ts
@@ -1,1 +1,7 @@
-// import {} from "@dticketin/common";
+import { Publisher, Subjects, TicketCreatedEvent } from "@dticketin/common";
+
+export class TicketCreatedPublisher extends Publisher<TicketCreatedEvent> {
+  subject: Subjects.TicketCreated = Subjects.TicketCreated;
+}
+
+// new TicketCreatedPublisher(client).publish(ticket)
